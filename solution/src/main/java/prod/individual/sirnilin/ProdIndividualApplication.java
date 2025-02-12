1c00825d36bf5733febd1066c20eff864f551620
Sirnilin <117184223+Sirnilin@users.noreply.github.com>
2025-02-12 14:45:48 +0300

    init

---
+++ solution/src/main/java/prod/individual/sirnilin/ProdIndividualApplication.java
@@ -0,0 +1,13 @@
+package prod.individual.sirnilin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdIndividualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdIndividualApplication.class, args);
+    }
+
+}
---
+++ solution/src/test/java/prod/individual/sirnilin/ProdIndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package prod.individual.sirnilin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdIndividualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
