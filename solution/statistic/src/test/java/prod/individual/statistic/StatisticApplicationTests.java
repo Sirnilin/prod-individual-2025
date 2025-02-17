398519282fba5ae2339ca823da25b22cb6e0daf4
Sirnilin <117184223+Sirnilin@users.noreply.github.com>
2025-02-17 13:31:33 +0300

    add Docker configuration for statistics service; set up PostgreSQL database and API container

---
+++ solution/statistic/src/test/java/prod/individual/statistic/StatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package prod.individual.statistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
